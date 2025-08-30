d3ad2a706a044d2831d3695ceee6ea0541ce6bb2
Paweł Fortuna <pawel.krzysztof.fortuna@outlook.com>
2025-08-31 00:36:54 +0200

    Async version of crate

--- src/interface.rs
+++ src/interface.rs
@@ -1,14 +1,10 @@
 //! I2C/UART interfaces
 
-use embedded_hal::{
-    blocking::i2c,
-    blocking::serial,
-    serial as serial_nb,
-};
-use nb::block;
+use embedded_hal_async::i2c;
+use embedded_io_async::{Read, Write};
 
-use crate::{Error, private};
 use crate::registers::*;
+use crate::{private, Error};
 
 /// I2C interface
 #[derive(Debug)]
@@ -28,42 +24,48 @@ pub trait WriteData: private::Sealed {
     /// Error type
     type Error;
     /// Write to an u8 register
-    fn write_register(&mut self, register: u8, data: u8) -> Result<(), Self::Error>;
+    fn write_register(&mut self, register: u8, data: u8) -> impl core::future::Future<Output = Result<(), Self::Error>>;
     /// Write data. The first element corresponds to the starting address.
-    fn write_data(&mut self, payload: u8) -> Result<(), Self::Error>;
+    fn write_data(&mut self, payload: u8) -> impl core::future::Future<Output = Result<(), Self::Error>>;
 }
 
 impl<I2C, E> WriteData for I2cInterface<I2C>
-    where
-        I2C: i2c::Write<Error=E>,
+where
+    I2C: i2c::I2c<Error = E>,
 {
     type Error = Error<E>;
-    fn write_register(&mut self, register: u8, data: u8) -> Result<(), Self::Error> {
+    async fn write_register(&mut self, register: u8, data: u8) -> Result<(), Self::Error> {
         let register = Commands::WReg as u8 | (register << 2); // write command
         self.i2c
-            .write(self.address, &[register, data])
+            .write(self.address, &[register, data]).await
             .map_err(Error::CommError)
     }
 
-    fn write_data(&mut self, payload: u8) -> Result<(), Self::Error> {
-        self.i2c.write(self.address, &[payload]).map_err(Error::CommError)
+    async fn write_data(&mut self, payload: u8) -> Result<(), Self::Error> {
+        self.i2c
+            .write(self.address, &[payload]).await
+            .map_err(Error::CommError)
     }
 }
 
 impl<UART, E> WriteData for SerialInterface<UART>
-    where
-        UART: serial::Write<u8, Error=E> + serial_nb::Read<u8, Error=E>,
+where
+    UART: Write<Error = E> + Read<Error = E>,
 {
     type Error = Error<E>;
-    fn write_register(&mut self, register: u8, data: u8) -> Result<(), Self::Error> {
+    async fn write_register(&mut self, register: u8, data: u8) -> Result<(), Self::Error> {
         let register = Commands::WReg as u8 | (register << 2); // write command
-        self.serial.bwrite_all(&[0x55, register, data]).map_err(Error::CommError)?;
-        self.serial.bflush().map_err(Error::CommError)
+        self.serial
+            .write_all(&[0x55, register, data]).await
+            .map_err(Error::CommError)?;
+        self.serial.flush().await.map_err(Error::CommError)
     }
 
-    fn write_data(&mut self, payload: u8) -> Result<(), Self::Error> {
-        self.serial.bwrite_all(&[0x55, payload]).map_err(Error::CommError)?;
-        self.serial.bflush().map_err(Error::CommError)
+    async fn write_data(&mut self, payload: u8) -> Result<(), Self::Error> {
+        self.serial
+            .write_all(&[0x55, payload]).await
+            .map_err(Error::CommError)?;
+        self.serial.flush().await.map_err(Error::CommError)
     }
 }
 
@@ -72,29 +74,29 @@ pub trait ReadData: private::Sealed {
     /// Error type
     type Error;
     /// Read an u8 register
-    fn read_register(&mut self, register: u8) -> Result<u8, Self::Error>;
+    fn read_register(&mut self, register: u8) -> impl core::future::Future<Output = Result<u8, Self::Error>>;
     /// Read some data. The first element corresponds to the starting address.
-    fn read_data(&mut self) -> Result<u32, Self::Error>;
+    fn read_data(&mut self) -> impl core::future::Future<Output = Result<u32, Self::Error>>;
 }
 
 impl<I2C, E> ReadData for I2cInterface<I2C>
-    where
-        I2C: i2c::WriteRead<Error=E>,
+where
+    I2C: i2c::I2c<Error = E>,
 {
     type Error = Error<E>;
-    fn read_register(&mut self, register: u8) -> Result<u8, Self::Error> {
+    async fn read_register(&mut self, register: u8) -> Result<u8, Self::Error> {
         let register = Commands::RReg as u8 | (register << 2); // read command
         let mut buffer = [0];
         self.i2c
-            .write_read(self.address, &[register], &mut buffer)
+            .write_read(self.address, &[register], &mut buffer).await
             .map(|_| buffer[0])
             .map_err(Error::CommError)
     }
 
-    fn read_data(&mut self) -> Result<u32, Self::Error> {
+    async fn read_data(&mut self) -> Result<u32, Self::Error> {
         let mut buffer = [0, 0, 0];
         self.i2c
-            .write_read(self.address, &[Commands::RData as u8], &mut buffer)
+            .write_read(self.address, &[Commands::RData as u8], &mut buffer).await
             .map(|_| {
                 let msb = buffer[0];
                 let csb = buffer[1];
@@ -106,23 +108,31 @@ impl<I2C, E> ReadData for I2cInterface<I2C>
 }
 
 impl<UART, E> ReadData for SerialInterface<UART>
-    where
-        UART: serial::Write<u8, Error=E> + serial_nb::Read<u8, Error=E>,
+where
+    UART: Write<Error = E> + Read<Error = E>,
 {
     type Error = Error<E>;
-    fn read_register(&mut self, register: u8) -> Result<u8, Self::Error> {
+    async fn read_register(&mut self, register: u8) -> Result<u8, Self::Error> {
         let register = Commands::RReg as u8 | (register << 2); // read command
-        self.serial.bwrite_all(&[0x55, register]).map_err(Error::CommError)?;
-        self.serial.bflush().map_err(Error::CommError)?;
-        block!(self.serial.read()).map_err(Error::CommError)
+        self.serial
+            .write_all(&[0x55, register]).await
+            .map_err(Error::CommError)?;
+        self.serial.flush().await.map_err(Error::CommError)?;
+
+        let mut out = [0];
+        self.serial.read(&mut out).await.map_err(Error::CommError)?;
+
+        Ok(out[0])
     }
 
-    fn read_data(&mut self) -> Result<u32, Self::Error> {
-        self.serial.bwrite_all(&[0x55, Commands::RData as u8]).map_err(Error::CommError)?;
-        self.serial.bflush().map_err(Error::CommError)?;
-        let msb = block!(self.serial.read()).map_err(Error::CommError)?;
-        let csb = block!(self.serial.read()).map_err(Error::CommError)?;
-        let lsb = block!(self.serial.read()).map_err(Error::CommError)?;
+    async fn read_data(&mut self) -> Result<u32, Self::Error> {
+        let mut out = [0, 0, 0];
+        self.serial
+            .write_all(&[0x55, Commands::RData as u8]).await
+            .map_err(Error::CommError)?;
+        self.serial.flush().await.map_err(Error::CommError)?;
+        self.serial.read(&mut out).await.map_err(Error::CommError)?;
+        let [msb, csb, lsb] = out;
         Ok((msb as u32) << 16 | (csb as u32) << 8 | (lsb as u32))
     }
 }
--- src/lib.rs
+++ src/lib.rs
@@ -8,18 +8,14 @@ use core::fmt::Debug;
 use core::result::Result;
 use core::result::Result::Err;
 
-use embedded_hal::{
-    blocking::i2c,
-    blocking::serial,
-    serial as serial_nb,
-};
+use embedded_hal_async::i2c;
+use embedded_io_async::{Read, Write};
 
-use crate::{interface::{I2cInterface, ReadData, SerialInterface, WriteData}};
+use crate::interface::{I2cInterface, ReadData, SerialInterface, WriteData};
 use crate::registers::*;
 
-pub mod registers;
 pub mod interface;
-
+pub mod registers;
 
 mod private {
     use super::interface;
@@ -28,13 +24,12 @@ mod private {
 
     impl<UART> Sealed for interface::SerialInterface<UART> {}
 
-    impl<I2C, > Sealed for interface::I2cInterface<I2C> {}
+    impl<I2C> Sealed for interface::I2cInterface<I2C> {}
 }
 
 #[derive(Debug, Eq, PartialEq, Copy, Clone)]
 /// Error enum for ADS122x04
-pub enum Error<E>
-{
+pub enum Error<E> {
     /// The input is shorted
     ShortedInput,
     /// Inputs is open
@@ -48,8 +43,7 @@ pub enum Error<E>
 }
 
 /// Device handler for ADS122x04
-pub struct ADS122x04<BUS>
-{
+pub struct ADS122x04<BUS> {
     bus: BUS,
     /// offset of the ADC
     pub offset: i32,
@@ -70,12 +64,11 @@ pub struct ADS122x04<BUS>
 }
 
 impl<I2C, E> ADS122x04<I2cInterface<I2C>>
-    where
-        I2C: i2c::Write<Error=E> + i2c::WriteRead<Error=E>,
+where
+    I2C: i2c::I2c<Error = E>,
 {
     /// Create a new ADS122C04 device by supplying an I2C address and I2C handler
-    pub fn new_i2c(address: u8, i2c: I2C) -> Self
-    {
+    pub fn new_i2c(address: u8, i2c: I2C) -> Self {
         ADS122x04 {
             bus: I2cInterface { i2c, address },
             offset: 0,
@@ -98,8 +91,8 @@ impl<I2C, E> ADS122x04<I2cInterface<I2C>>
 }
 
 impl<UART, E> ADS122x04<SerialInterface<UART>>
-    where
-        UART: serial::Write<u8, Error=E> + serial_nb::Read<u8, Error=E>,
+where
+    UART: Write<Error = E> + Read<Error = E>,
 {
     /// Create a new ADS122C04 device by supplying a serial handler (UART)
     pub fn new_serial(serial: UART) -> Self {
@@ -125,15 +118,16 @@ impl<UART, E> ADS122x04<SerialInterface<UART>>
 }
 
 impl<BUS, E> ADS122x04<BUS>
-    where
-        BUS: ReadData<Error=Error<E>> + WriteData<Error=Error<E>>,
+where
+    BUS: ReadData<Error = Error<E>> + WriteData<Error = Error<E>>,
 {
     /// updates a specified config register
-    fn update_reg(&mut self, reg: u8) -> Result<(), Error<E>> {
+    async fn update_reg(&mut self, reg: u8) -> Result<(), Error<E>> {
         match reg {
             0x00 => {
-                let val = (self.pga_bypass as u8) | ((self.gain as u8) << 1) | ((self.mux as u8) << 4);
-                self.bus.write_register(0x00, val)
+                let val =
+                    (self.pga_bypass as u8) | ((self.gain as u8) << 1) | ((self.mux as u8) << 4);
+                self.bus.write_register(0x00, val).await
             }
             0x01 => {
                 let val = (self.temperature_sensor_mode as u8)
@@ -141,223 +135,224 @@ impl<BUS, E> ADS122x04<BUS>
                     | ((self.conversion_mode as u8) << 3)
                     | ((self.turbo_mode as u8) << 4)
                     | ((self.data_rate as u8) << 5);
-                self.bus.write_register(0x01, val)
+                self.bus.write_register(0x01, val).await
             }
             0x02 => {
                 let val = (self.current_source as u8)
                     | ((self.burn_out_current_sources as u8) << 3)
                     | ((self.crc as u8) << 4)
                     | ((self.data_counter_enable as u8) << 6);
-                self.bus.write_register(0x02, val)
+                self.bus.write_register(0x02, val).await
             }
             0x03 => {
                 let val = ((self.current_route_2 as u8) << 2) | ((self.current_route_1 as u8) << 5);
-                self.bus.write_register(0x03, val)
+                self.bus.write_register(0x03, val).await
             }
             _ => Err(Error::InvalidValue),
         }
     }
 
     /// reads a specified config register
-    fn read_reg(&mut self, reg: u8) -> Result<u8, Error<E>> {
+    async fn read_reg(&mut self, reg: u8) -> Result<u8, Error<E>> {
         match reg {
-            0x00 => self.bus.read_register(0x00),
-            0x01 => self.bus.read_register(0x01),
-            0x02 => self.bus.read_register(0x02),
-            0x03 => self.bus.read_register(0x03),
+            0x00 => self.bus.read_register(0x00).await,
+            0x01 => self.bus.read_register(0x01).await,
+            0x02 => self.bus.read_register(0x02).await,
+            0x03 => self.bus.read_register(0x03).await,
             _ => Err(Error::InvalidValue),
         }
     }
 
     /// Calibrate the offset (according to 8.3.11 Offset Calibration in datasheet)
     /// This is recommended upon startup and after changing the gain.
-    pub fn calibrate_offset(&mut self) -> Result<(), Error<E>> {
+    pub async fn calibrate_offset(&mut self) -> Result<(), Error<E>> {
         const NUM_AVG: usize = 10;
         let timeout = 1000;
         // short the inputs to mid-supply (AVDD + AVSS) / 2
         let previous_mux = self.mux;
-        self.set_input_mux(Mux::Shorted)?;
-        self.set_data_rate(DataRate::Sps40Turbo)?;
-        self.set_conversion_mode(ConversionMode::SingleShot)?;
+        self.set_input_mux(Mux::Shorted).await?;
+        self.set_data_rate(DataRate::Sps40Turbo).await?;
+        self.set_conversion_mode(ConversionMode::SingleShot).await?;
         // reset offset
         self.offset = 0;
         // take multiple readings and average
         let mut offset = 0;
         let mut timeout_counter = 0;
         for _ in 0..NUM_AVG {
-            self.start()?;
-            while !self.get_data_ready()? {
+            self.start().await?;
+            while !self.get_data_ready().await? {
                 timeout_counter += 1;
                 if timeout_counter > timeout {
                     return Err(Error::Timeout);
                 }
             }
-            offset += self.get_raw_adc()?;
+            offset += self.get_raw_adc().await?;
         }
         // store offset
         self.offset = offset / (NUM_AVG as i32);
         // return to previous mux
-        self.set_input_mux(previous_mux)?;
+        self.set_input_mux(previous_mux).await?;
         Ok(())
     }
 
     /// Enable or disable the programmable gain amplifier (PGA)
-    pub fn set_pga_bypass(&mut self, state: bool) -> Result<(), Error<E>> {
+    pub async fn set_pga_bypass(&mut self, state: bool) -> Result<(), Error<E>> {
         self.pga_bypass = state;
-        self.update_reg(0x00)
+        self.update_reg(0x00).await
     }
 
     /// Read the status of the programmable gain amplifier (PGA)
-    pub fn get_pga_bypass(&mut self) -> Result<bool, Error<E>> {
-        self.read_reg(0x00).map(|val| (val & 0b1) == 1)
+    pub async fn get_pga_bypass(&mut self) -> Result<bool, Error<E>> {
+        self.read_reg(0x00).await.map(|val| (val & 0b1) == 1)
     }
 
     /// Set the gain as either 0, 1, 2, 4, 8, 16, 32, 64 or 128
-    pub fn set_gain(&mut self, gain: Gain) -> Result<(), Error<E>> {
+    pub async fn set_gain(&mut self, gain: Gain) -> Result<(), Error<E>> {
         self.gain = gain;
-        self.update_reg(0x00)
+        self.update_reg(0x00).await
     }
 
     /// Read the gain value
-    pub fn get_gain(&mut self) -> Result<Gain, Error<E>> {
-        self.read_reg(0x00).map(|val| Gain::from((val >> 1) & 0b111))
+    pub async fn get_gain(&mut self) -> Result<Gain, Error<E>> {
+        self.read_reg(0x00).await
+            .map(|val| Gain::from((val >> 1) & 0b111))
     }
 
     /// Set the input multiplexer (MUX)
-    pub fn set_input_mux(&mut self, mux: Mux) -> Result<(), Error<E>> {
+    pub async fn set_input_mux(&mut self, mux: Mux) -> Result<(), Error<E>> {
         self.mux = mux;
-        self.update_reg(0x00)
+        self.update_reg(0x00).await
     }
 
     /// Read the input multiplexer (MUX) setting
-    pub fn get_input_mux(&mut self) -> Result<u8, Error<E>> {
-        self.read_reg(0x00).map(|val| val >> 4)
+    pub async fn get_input_mux(&mut self) -> Result<u8, Error<E>> {
+        self.read_reg(0x00).await.map(|val| val >> 4)
     }
 
     /// Enable or disable temperature sensor mode (TS)
-    pub fn set_temperature_sensor_mode(&mut self, state: bool) -> Result<(), Error<E>> {
+    pub async fn set_temperature_sensor_mode(&mut self, state: bool) -> Result<(), Error<E>> {
         self.temperature_sensor_mode = state;
-        self.update_reg(0x01)
+        self.update_reg(0x01).await
     }
 
     /// Read the temperature sensor mode (TS)
-    pub fn get_temperature_sensor_mode(&mut self) -> Result<bool, Error<E>> {
-        self.read_reg(0x01).map(|val| (val & 0b1) == 1)
+    pub async fn get_temperature_sensor_mode(&mut self) -> Result<bool, Error<E>> {
+        self.read_reg(0x01).await.map(|val| (val & 0b1) == 1)
     }
 
     /// Set the voltage reference (VREF)
-    pub fn set_vref(&mut self, v_ref: VRef) -> Result<(), Error<E>> {
+    pub async fn set_vref(&mut self, v_ref: VRef) -> Result<(), Error<E>> {
         self.v_ref = v_ref;
-        self.update_reg(0x01)
+        self.update_reg(0x01).await
     }
 
     /// Read the voltage reference (VREF)
-    pub fn get_vref(&mut self) -> Result<VRef, Error<E>> {
-        self.read_reg(0x01)
+    pub async fn get_vref(&mut self) -> Result<VRef, Error<E>> {
+        self.read_reg(0x01).await
             .map(|val| VRef::from((val >> 1) & 0b11, self.v_ref.to_voltage()))
     }
 
     /// Set the conversion mode (CM)
-    pub fn set_conversion_mode(&mut self, mode: ConversionMode) -> Result<(), Error<E>> {
+    pub async fn set_conversion_mode(&mut self, mode: ConversionMode) -> Result<(), Error<E>> {
         self.conversion_mode = mode;
-        self.update_reg(0x01)
+        self.update_reg(0x01).await
     }
 
     /// Read the conversion mode (CM)
-    pub fn get_conversion_mode(&mut self) -> Result<ConversionMode, Error<E>> {
-        self.read_reg(0x01)
+    pub async fn get_conversion_mode(&mut self) -> Result<ConversionMode, Error<E>> {
+        self.read_reg(0x01).await
             .map(|val| ConversionMode::from((val >> 3) & 0b1))
     }
 
     /// Read the operating mode
-    pub fn get_operating_mode(&mut self) -> Result<bool, Error<E>> {
-        self.read_reg(0x01).map(|val| ((val >> 4) & 0b1) == 1)
+    pub async fn get_operating_mode(&mut self) -> Result<bool, Error<E>> {
+        self.read_reg(0x01).await.map(|val| ((val >> 4) & 0b1) == 1)
     }
 
     /// Set the data rate
-    pub fn set_data_rate(&mut self, rate: DataRate) -> Result<(), Error<E>> {
+    pub async fn set_data_rate(&mut self, rate: DataRate) -> Result<(), Error<E>> {
         self.data_rate = rate;
         self.turbo_mode = (self.data_rate as u8 & 0b1) == 1;
-        self.update_reg(0x01)
+        self.update_reg(0x01).await
     }
 
     /// Read the data rate
-    pub fn get_data_rate(&mut self) -> Result<DataRate, Error<E>> {
-        self.read_reg(0x01)
+    pub async fn get_data_rate(&mut self) -> Result<DataRate, Error<E>> {
+        self.read_reg(0x01).await
             .map(|val| DataRate::from((val >> 4) & 0b1111))
     }
 
     /// Set the current level of the internal excitation current sources
-    pub fn set_current_level(&mut self, current: CurrentSource) -> Result<(), Error<E>> {
+    pub async fn set_current_level(&mut self, current: CurrentSource) -> Result<(), Error<E>> {
         self.current_source = current;
-        self.update_reg(0x02)
+        self.update_reg(0x02).await
     }
 
     /// Read the current level of the internal excitation current sources
-    pub fn get_current_level(&mut self) -> Result<CurrentSource, Error<E>> {
-        self.read_reg(0x02)
+    pub async fn get_current_level(&mut self) -> Result<CurrentSource, Error<E>> {
+        self.read_reg(0x02).await
             .map(|val| CurrentSource::from(val & 0b111))
     }
 
     /// Enable or disable the 10 uA burnout current sources
-    pub fn set_burnout_current_source(&mut self, state: bool) -> Result<(), Error<E>> {
+    pub async fn set_burnout_current_source(&mut self, state: bool) -> Result<(), Error<E>> {
         self.burn_out_current_sources = state;
-        self.update_reg(0x02)
+        self.update_reg(0x02).await
     }
 
     /// Read the state of the 10 uA burnout current sources
-    pub fn get_burnout_current_source(&mut self) -> Result<bool, Error<E>> {
-        self.read_reg(0x02).map(|val| ((val >> 3) & 0b1) == 1)
+    pub async fn get_burnout_current_source(&mut self) -> Result<bool, Error<E>> {
+        self.read_reg(0x02).await.map(|val| ((val >> 3) & 0b1) == 1)
     }
 
     /// Set the CRC mode
-    pub fn set_crc(&mut self, crc: Crc) -> Result<(), Error<E>> {
+    pub async fn set_crc(&mut self, crc: Crc) -> Result<(), Error<E>> {
         self.crc = crc;
-        self.update_reg(0x02)
+        self.update_reg(0x02).await
     }
 
     /// Read the CRC mode
-    pub fn get_crc(&mut self) -> Result<Crc, Error<E>> {
-        self.read_reg(0x02).map(|val| Crc::from((val >> 4) & 0b11))
+    pub async fn get_crc(&mut self) -> Result<Crc, Error<E>> {
+        self.read_reg(0x02).await.map(|val| Crc::from((val >> 4) & 0b11))
     }
 
     /// Enable or disable data counter
-    pub fn set_data_counter(&mut self, state: bool) -> Result<(), Error<E>> {
+    pub async fn set_data_counter(&mut self, state: bool) -> Result<(), Error<E>> {
         self.data_counter_enable = state;
-        self.update_reg(0x02)
+        self.update_reg(0x02).await
     }
 
     /// Read the state of the data counter
-    pub fn get_data_counter(&mut self) -> Result<bool, Error<E>> {
-        self.read_reg(0x02).map(|val| ((val >> 6) & 0b1) == 1)
+    pub async fn get_data_counter(&mut self) -> Result<bool, Error<E>> {
+        self.read_reg(0x02).await.map(|val| ((val >> 6) & 0b1) == 1)
     }
 
     /// Read the data ready (DRDY) register
-    pub fn get_data_ready(&mut self) -> Result<bool, Error<E>> {
-        self.read_reg(0x02).map(|val| ((val >> 7) & 0b1) == 1)
+    pub async fn get_data_ready(&mut self) -> Result<bool, Error<E>> {
+        self.read_reg(0x02).await.map(|val| ((val >> 7) & 0b1) == 1)
     }
 
     /// Set the current routing of the excitation current source 1
-    pub fn set_current_route_1(&mut self, route: CurrentRoute) -> Result<(), Error<E>> {
+    pub async fn set_current_route_1(&mut self, route: CurrentRoute) -> Result<(), Error<E>> {
         self.current_route_1 = route;
-        self.update_reg(0x03)
+        self.update_reg(0x03).await
     }
 
     /// Read the current routing of the excitation current source 1
-    pub fn get_current_route_1(&mut self) -> Result<CurrentRoute, Error<E>> {
-        self.read_reg(0x03)
+    pub async fn get_current_route_1(&mut self) -> Result<CurrentRoute, Error<E>> {
+        self.read_reg(0x03).await
             .map(|val| CurrentRoute::from((val >> 5) & 0b111))
     }
 
     /// Set the current routing of the excitation current source 2
-    pub fn set_current_route_2(&mut self, route: CurrentRoute) -> Result<(), Error<E>> {
+    pub async fn set_current_route_2(&mut self, route: CurrentRoute) -> Result<(), Error<E>> {
         self.current_route_2 = route;
-        self.update_reg(0x03)
+        self.update_reg(0x03).await
     }
 
     /// Read the current routing of the excitation current source 2
-    pub fn get_current_route_2(&mut self) -> Result<CurrentRoute, Error<E>> {
-        self.read_reg(0x03)
+    pub async fn get_current_route_2(&mut self) -> Result<CurrentRoute, Error<E>> {
+        self.read_reg(0x03).await
             .map(|val| CurrentRoute::from((val >> 3) & 0b111))
     }
 
@@ -371,14 +366,16 @@ impl<BUS, E> ADS122x04<BUS>
     }
 
     /// Read the raw ADC value and subtract the offset
-    pub fn get_raw_adc(&mut self) -> Result<i32, Error<E>> {
-        self.bus.read_data().map(|val| self.raw_to_signed(val) - self.offset)
+    pub async fn get_raw_adc(&mut self) -> Result<i32, Error<E>> {
+        self.bus
+            .read_data().await
+            .map(|val| self.raw_to_signed(val) - self.offset)
     }
 
     /// Read the voltage of the ADC
-    pub fn get_voltage(&mut self) -> Result<f32, Error<E>> {
+    pub async fn get_voltage(&mut self) -> Result<f32, Error<E>> {
         // returns voltage in V
-        let raw = self.get_raw_adc();
+        let raw = self.get_raw_adc().await;
         let v_ref = self.v_ref.to_voltage();
         raw.map(|raw| (v_ref as f64 / ((1 << 23) as f64) * (raw as f64)) as f32)
     }
@@ -391,12 +388,12 @@ impl<BUS, E> ADS122x04<BUS>
     }
 
     /// Reset the device
-    pub fn reset(&mut self) -> Result<(), Error<E>> {
-        self.bus.write_data(Commands::Reset as u8)
+    pub async fn reset(&mut self) -> Result<(), Error<E>> {
+        self.bus.write_data(Commands::Reset as u8).await
     }
 
     /// Start a measurement
-    pub fn start(&mut self) -> Result<(), Error<E>> {
-        self.bus.write_data(Commands::StartSync as u8)
+    pub async fn start(&mut self) -> Result<(), Error<E>> {
+        self.bus.write_data(Commands::StartSync as u8).await
     }
 }
